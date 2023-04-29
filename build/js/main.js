3451f52a689540178b670e7a20c9b5695b1831b2
Abdul Majid <76196721+majid-2002@users.noreply.github.com>
2023-04-29 14:09:05 +0530

    Chapter 3 , arrays, object, enums, interface

--- build/js/main.js
+++ build/js/main.js
@@ -1,17 +1,54 @@
 "use strict";
-let myName = 'majid';
-let meaningOfLIfe;
-let isLoading;
-let album; //? any type
-myName = "Jhon";
-meaningOfLIfe = 42;
-isLoading = true;
-album = 1979;
-//? (): number => is the return type of the function
-const sum = (a, b) => {
-    return a + b;
+let stringArr = ["one", "two", "three"];
+let guitars = ["one", 2, "three"];
+let mixedArray = ["one", 2, true];
+stringArr[0] = "John";
+stringArr.push("hey");
+guitars[0] = 1984;
+guitars.unshift("Jim");
+let test = [];
+let bands = [];
+bands.push("One direction");
+//Tuple
+let myTuple = ["majid", 42, true];
+let mixed = ["John", 1, false];
+myTuple[1] = 12;
+//objects
+let myObj;
+//? an object can be a [] or {}
+myObj = [];
+myObj = bands;
+myObj = {};
+const exampleObj = {
+    prop1: "Majid",
+    prop2: true,
 };
-let postId;
-let isActive;
-//? regular expression
-let re = /\w+/g;
+exampleObj.prop1 = "John";
+const evh = {
+    name: "Eddie",
+    active: false,
+    albums: [1984, 5153, "OU812"],
+};
+let jp = {
+    name: "Jimmy",
+    active: true,
+    albums: [1989, "I"],
+};
+//we declared function with Guitarist type
+const greetGuitarist = (guitarist) => {
+    if (guitarist.name)
+        return `Hello ${guitarist.name.toUpperCase()}`;
+    return `Hello..`;
+};
+console.log(greetGuitarist(jp));
+// Enums
+// Enums is additional feature in typscript that is not in javascript
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["c"] = 3] = "c";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.A);
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,85 @@
-let myName = 'majid'
-let meaningOfLIfe: number;
-let isLoading: boolean;
-let album: any; //? any type
+let stringArr = ["one", "two", "three"];
 
-myName = "Jhon"
-meaningOfLIfe = 42
-isLoading = true
-album = 1979
+let guitars = ["one", 2, "three"];
 
+let mixedArray = ["one", 2, true];
 
+stringArr[0] = "John";
+stringArr.push("hey");
 
-//? (): number => is the return type of the function
-const sum = (a: number, b: number): number => {
-    return a + b
+guitars[0] = 1984;
+guitars.unshift("Jim");
+
+let test = [];
+let bands: string[] = [];
+bands.push("One direction");
+
+//Tuple
+let myTuple: [string, number, boolean] = ["majid", 42, true];
+let mixed = ["John", 1, false];
+
+myTuple[1] = 12;
+
+//objects
+let myObj: object;
+
+//? an object can be a [] or {}
+myObj = [];
+myObj = bands;
+myObj = {};
+
+const exampleObj = {
+  prop1: "Majid",
+  prop2: true,
+};
+
+exampleObj.prop1 = "John";
+
+//interface can also be used just like type
+
+//interface Guitarist {
+//   name: string;
+//   active?: boolean; //optional
+//   albums: (string | number)[];
+// }
+
+//object with a type
+type Guitarist = {
+  name?: string; //optional
+  active?: boolean; //optional
+  albums: (string | number)[];
+};
+
+const evh: Guitarist = {
+  name: "Eddie",
+  active: false,
+  albums: [1984, 5153, "OU812"],
+};
+
+let jp: Guitarist = {
+  name: "Jimmy",
+  active: true,
+  albums: [1989, "I"],
+};
+
+//we declared function with Guitarist type
+const greetGuitarist = (guitarist: Guitarist) => {
+  if (guitarist.name) return `Hello ${guitarist.name.toUpperCase()}`;
+
+  return `Hello..`;
+};
+
+console.log(greetGuitarist(jp));
+
+// Enums
+// Enums is additional feature in typscript that is not in javascript
+
+enum Grade {
+  U = 1,
+  D,
+  c,
+  B,
+  A,
 }
 
-
-let postId : string | number;
-let isActive : boolean | string;
-
-//? regular expression
-let re: RegExp = /\w+/g;
+console.log(Grade.A);
