b49553ea1a2e3196dabdc79f32d833e7f596045e
Abdul Majid <76196721+majid-2002@users.noreply.github.com>
2023-04-30 10:09:24 +0530

    Chapter 6: Classes , static, setter, getter, accessmodifiers

--- build/js/main.js
+++ build/js/main.js
@@ -1,11 +1,81 @@
 "use strict";
-// convert to more or less specific
-let a = "world";
-let b = a; // less specific
-let c = a; //more specific
-let d = "hai";
-let e = "hello";
-10;
-// The DOM
-const img = document.getElementById("img");
-img.src;
+class Coder {
+    constructor(name, music, age, lang = "Typescript") {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+        this.name = name;
+        this.age = age;
+        this.music = music;
+        this.lang = lang;
+    }
+    getAge() {
+        return `Hello I am ${this.age}`;
+    }
+}
+const Majid = new Coder("majid", "Rock", 21); //? object of class
+class WebDev extends Coder {
+    constructor(computer, name, music, age) {
+        super(name, music, age);
+        this.computer = computer;
+        this.computer = computer;
+    }
+    //? lang is acceessible in its child class but the age is not accessible in its child class on
+    getLang() {
+        return `My language is ${this.lang}`;
+    }
+}
+const Majid2 = new WebDev("Mac", "majid", "Lofi", 23); //? object of class
+class Guitarist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} ${action} the ${this.instrument}`;
+    }
+}
+const Page = new Guitarist("Jimmy", "Guitar");
+// console.log(Page.play("strums"));
+////////////////////////////////////////
+class Peeps {
+    static getCount() {
+        return this.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+}
+Peeps.count = 0;
+const John = new Peeps("John");
+const Paul = new Peeps("Paul");
+const Ringo = new Peeps("Ringo");
+// console.log(John.id);
+// console.log(Paul.id);
+// console.log(Ringo.id);
+// console.log(Peeps.count);
+class Bands {
+    constructor() {
+        this.dataState = [];
+    }
+    get data() {
+        return this.dataState;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.every((el) => typeof el === "string")) {
+            this.dataState = value;
+        }
+        else
+            throw new Error("Param is not array of strings");
+    }
+}
+const MyBands = new Bands();
+//setter
+MyBands.data = ["The Beatles", "The Who", "The Kinks"];
+//getter
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, "The Doors"];
+console.log(MyBands.data);
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,122 @@
-type One = string;
-type Two = string | number;
-type Three = 'hai'
+class Coder {
+  secondLang!: string;
 
-// convert to more or less specific
-let a: One = "world";
-let b = a as Two; // less specific
-let c = a as Three; //more specific
+  constructor(
+    public readonly name: string,
+    public music: string,
+    private age: number,
+    protected lang: string = "Typescript"
+  ) {
+    this.name = name;
+    this.age = age;
+    this.music = music;
+    this.lang = lang;
+  }
 
+  public getAge() {
+    return `Hello I am ${this.age}`;
+  }
+}
 
-let d = <One>"hai";
-let e = <string | number>"hello";
+const Majid = new Coder("majid", "Rock", 21); //? object of class
 
+class WebDev extends Coder {
+  constructor(
+    public computer: string,
+    name: string,
+    music: string,
+    age: number
+  ) {
+    super(name, music, age);
+    this.computer = computer;
+  }
 
-(10 as unknown) as string;
+  //? lang is acceessible in its child class but the age is not accessible in its child class on
+  public getLang() {
+    return `My language is ${this.lang}`;
+  }
+}
 
+const Majid2 = new WebDev("Mac", "majid", "Lofi", 23); //? object of class
 
+// console.log(Majid2.getLang());
+// console.log(Majid2.age);
+// console.log(Majid2.lang);
 
-// The DOM
-const img = document.getElementById("img")! as HTMLImageElement
+/////////////////////////// Interaces ///////////////////////////
 
-img.src
+interface Musician {
+  name: string;
+  instrument: string;
+  play(action: string): string;
+}
+
+class Guitarist implements Musician {
+  name: string;
+  instrument: string;
+
+  constructor(name: string, instrument: string) {
+    this.name = name;
+    this.instrument = instrument;
+  }
+
+  play(action: string) {
+    return `${this.name} ${action} the ${this.instrument}`;
+  }
+}
+
+const Page = new Guitarist("Jimmy", "Guitar");
+// console.log(Page.play("strums"));
+
+////////////////////////////////////////
+
+class Peeps {
+  static count: number = 0;
+
+  static getCount() {
+    return this.count;
+  }
+
+  public id: number;
+
+  constructor(public name: string) {
+    this.name = name;
+    this.id = ++Peeps.count;
+  }
+}
+
+const John = new Peeps("John");
+const Paul = new Peeps("Paul");
+const Ringo = new Peeps("Ringo");
+
+// console.log(John.id);
+// console.log(Paul.id);
+// console.log(Ringo.id);
+// console.log(Peeps.count);
+
+class Bands {
+  private dataState: string[];
+
+  constructor() {
+    this.dataState = [];
+  }
+
+  public get data(): string[] {
+    return this.dataState;
+  }
+
+  public set data(value: string[]) {
+    if (Array.isArray(value) && value.every((el) => typeof el === "string")) {
+      this.dataState = value;
+    } else throw new Error("Param is not array of strings");
+  }
+}
+
+const MyBands = new Bands();
+
+//setter
+MyBands.data = ["The Beatles", "The Who", "The Kinks"];
+//getter
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, "The Doors"];
+console.log(MyBands.data);
