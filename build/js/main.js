86fd02500d067de6b14fc379762413faeb66f9d2
Abdul Majid <76196721+majid-2002@users.noreply.github.com>
2023-04-30 13:04:25 +0530

    Chapter 8: Add generic functions, interface, and class with examples Writing a generic function Using a generic function to check if a value is an object Using a generic function to check if a value is truthy or falsy Defining an interface with a generic type parameter Using a generic type parameter to create a function that operates on objects with an ID property Using a generic type parameter and a key parameter to create a function that retrieves a property from an array of objects Defining a class with a generic type parameter and creating instances with different data types.

--- build/js/main.js
+++ build/js/main.js
@@ -1,42 +1,47 @@
 "use strict";
-const todaysTransactions = {
-    Pizza: 20,
-    Books: 10,
-    Job: 30,
+// Generic function that accepts an argument of any type and returns the same type of argument
+const echo = (arg) => arg;
+// Generic function that checks if an argument is an object but not an array or null
+const isObj = (arg) => {
+    return typeof arg === "object" && !Array.isArray(arg) && arg !== null;
 };
-console.log(todaysTransactions.Pizza); // Accessing a value using dot notation
-let prop = "Pizza";
-console.log(todaysTransactions[prop]); // Accessing a value using square bracket notation
-const todaysNet = (transactions) => {
-    let total = 0;
-    for (const transaction in transactions) { // Looping through all keys in the object
-        total += transactions[transaction]; // Accessing a value using square bracket notation
+// Generic function that checks if an argument is truthy or falsy
+const isTrue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg, is: false };
     }
-    return total;
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { arg, is: false };
+    }
+    return { arg, is: !!arg };
 };
-console.log(todaysNet(todaysTransactions));
-//todaysTransactions.Pizza = 40 // Error: Cannot assign to 'Pizza' because it is a read-only property
-console.log(todaysTransactions['Dave']); // Undefined because 'Dave' is not a key in the object
-const student = {
-    name: "Doug",
-    GPA: 3.5,
-    classes: [100, 200]
+// Generic function that processes a user object that has an 'id' property
+const processUser = (user) => {
+    // Process the user with logic here
+    return user;
 };
-for (const key in student) { // Looping through all keys in the object
-    console.log(`${key}: ${student[key]}`); // Accessing a value using square bracket notation
+// Generic function that accepts an array of objects with 'id' properties and a key to return
+const getUsersProperty = (users, key) => {
+    // Map through the users and return the specified key for each user
+    return users.map((user) => user[key]);
+};
+// Generic class with a private data property of type T, and getter and setter methods for the data
+class StateObject {
+    constructor(value) {
+        this.data = value;
+    }
+    get state() {
+        return this.data;
+    }
+    set state(value) {
+        this.data = value;
+    }
 }
-Object.keys(student).map(key => {
-    console.log(student[key]); // Accessing a value using square bracket notation
-});
-const logStudentKey = (student, key) => {
-    console.log(`Student ${key}: ${student[key]}`); // Accessing a value using square bracket notation
-};
-logStudentKey(student, 'name');
-const monthlyIncomes = {
-    salary: 500,
-    bonus: 100,
-    sidehustle: 250
-};
-for (const revenue in monthlyIncomes) { // Looping through all keys in the object
-    console.log(monthlyIncomes[revenue]); // Accessing a value using square bracket notation
-}
+// Create a new instance of the StateObject class with a string value, and update its value using the setter method
+const store = new StateObject("John");
+console.log(store.state);
+store.state = "Dave";
+// Create a new instance of the StateObject class with an array of string, number, and boolean values, and update its value using the setter method
+const myState = new StateObject([15]);
+myState.state = ["Dave", 42, true];
+console.log(myState.state);
--- src/main.ts
+++ src/main.ts
@@ -1,80 +1,64 @@
-//Index Signatures
-interface TransactionObj {
-  readonly [index: string]: number; // An index signature that specifies that all keys should have a value of type number
-  Pizza: number;
-  Books: number;
-  Job: number;
-}
+// Generic function that accepts an argument of any type and returns the same type of argument
+const echo = <T>(arg: T): T => arg;
 
-const todaysTransactions: TransactionObj = {
-  Pizza: 20,
-  Books: 10,
-  Job: 30,
+// Generic function that checks if an argument is an object but not an array or null
+const isObj = <T>(arg: T): boolean => {
+  return typeof arg === "object" && !Array.isArray(arg) && arg !== null;
 };
 
-console.log(todaysTransactions.Pizza); // Accessing a value using dot notation
-let prop = "Pizza";
-console.log(todaysTransactions[prop]); // Accessing a value using square bracket notation
-
-const todaysNet = (transactions: TransactionObj): number => {
-  let total = 0;
-  for (const transaction in transactions) { // Looping through all keys in the object
-    total += transactions[transaction]; // Accessing a value using square bracket notation
+// Generic function that checks if an argument is truthy or falsy
+const isTrue = <T>(arg: T): { arg: T; is: boolean } => {
+  if (Array.isArray(arg) && !arg.length) {
+    return { arg, is: false };
   }
-  return total;
+  if (isObj(arg) && !Object.keys(arg as keyof T).length) {
+    return { arg, is: false };
+  }
+  return { arg, is: !!arg };
 };
 
-console.log(todaysNet(todaysTransactions));
-
-//todaysTransactions.Pizza = 40 // Error: Cannot assign to 'Pizza' because it is a read-only property
-
-console.log(todaysTransactions['Dave']) // Undefined because 'Dave' is not a key in the object
-
-///////////////////////////////////
-
-interface Student {
-  name: string;
-  GPA: number;
-  classes?: number[];
+// Generic interface that requires an 'id' property of type number
+interface HasID {
+  id: number;
 }
 
-const student: Student = {
-  name: "Doug",
-  GPA: 3.5,
-  classes: [100, 200]
+// Generic function that processes a user object that has an 'id' property
+const processUser = <T extends HasID>(user: T): T => {
+  // Process the user with logic here
+  return user;
+};
+
+// Generic function that accepts an array of objects with 'id' properties and a key to return
+const getUsersProperty = <T extends HasID, K extends keyof T>(
+  users: T[],
+  key: K
+): T[K][] => {
+  // Map through the users and return the specified key for each user
+  return users.map((user) => user[key]);
+};
+
+// Generic class with a private data property of type T, and getter and setter methods for the data
+class StateObject<T> {
+  private data: T;
+  constructor(value: T) {
+    this.data = value;
+  }
+
+  get state(): T {
+    return this.data;
+  }
+
+  set state(value: T) {
+    this.data = value;
+  }
 }
 
-for (const key in student) { // Looping through all keys in the object
-  console.log(`${key}: ${student[key as keyof Student]}`) // Accessing a value using square bracket notation
-}
+// Create a new instance of the StateObject class with a string value, and update its value using the setter method
+const store = new StateObject("John");
+console.log(store.state);
+store.state = "Dave";
 
-Object.keys(student).map(key => {
-  console.log(student[key as keyof typeof student]) // Accessing a value using square bracket notation
-})
-
-const logStudentKey = (student: Student, key: keyof Student): void => {
-  console.log(`Student ${key}: ${student[key]}`) // Accessing a value using square bracket notation
-}
-
-logStudentKey(student, 'name')
-
-
-//////////////////////////////////////////////////////////////////////
-// interface Incomes {
-//   [key: string]: number
-
-// }
-
-
-type Streams = 'salary' | 'bonus' | 'sidehustle' // Defining a union type of string literals
-type Incomes = Record<Streams, number> // Using Record utility type to create a new mapped type where all keys in the Streams union have a corresponding value of type number
-
-const monthlyIncomes: Incomes = {
-    salary: 500,
-    bonus: 100,
-    sidehustle: 250
-}
-
-for (const revenue in monthlyIncomes) { // Looping through all keys in the object
-    console.log(monthlyIncomes[revenue as keyof Incomes]) // Accessing a value using square bracket notation
-}
+// Create a new instance of the StateObject class with an array of string, number, and boolean values, and update its value using the setter method
+const myState = new StateObject<(string | number | boolean)[]>([15]);
+myState.state = ["Dave", 42, true];
+console.log(myState.state);
